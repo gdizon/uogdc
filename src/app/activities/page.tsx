e6b2528b9ca1413bbdec715b904d45b344643282
gdizon <lainegelic@gmail.com>
2025-01-14 15:20:28 -0500

    going somewhere

---
+++ src/app/activities/page.tsx
@@ -0,0 +1,5 @@
+export default function Activities() {
+    return(<>
+        <h1>This is where the activities carousel will go.</h1>
+    </>);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,46 @@
-export default function Home() {
-  return (
-    <p>Hello uOGDC</p>
-  );
+import { products } from '@/utils/products';
+import '../../public/css/style.css';
+
+
+export default function Home(){
+    return (<>
+    
+        <h1 className='text-centre'>Welcome to the uOttawa Game Development Club website!</h1>
+    
+    </>)
 }
+
+//---------------------------------------------------------------------------
+//
+//        LearnA    Just for test/learning   //   DELETE
+//
+//---------------------------------------------------------------------------
+// (<>
+//     <h1>Welcome to the uOttawa Game Development Club website!</h1>
+//     <table>
+//         <thead>
+//             <tr>
+//                 <th>Name</th>
+//                 <th>Brand</th>
+//                 <th>Price</th>
+//             </tr>
+//         </thead>
+//         <tbody>
+//             {
+//                 products.map( item => (
+//                     <tr key={item.id}>
+                        
+//                     </tr>
+//                 ))
+//             }
+//         </tbody>
+//     </table>
+//     <ol>
+//         {
+//             products.map(item => (
+//                 <li key={item.id}> {item.name}</li>
+//             ))
+//         }
+//     </ol>
+// </>)
+//--------------------------------------------------------------------
