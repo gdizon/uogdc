0a30d9213fad1a3f582180f688b1518345880bba
gdizon <lainegelic@gmail.com>
2025-01-29 15:54:20 -0500

    nav bar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,26 +1,29 @@
-// import { Section } from 'lucide-react';
 import React from 'react';
-// import localFont from 'next/font/local';
 import './home.css'
+import { Button } from '@/components/ui/button';
 
-// const pixeledfont = localFont({
-//     src: [{
-//         path: '../fonts/Pixeled.ttf'
-//     }]
-// })
 
 const Home = () => {
-    const scrollToSection = (sectionId: string) => {
-        const element = document.getElementById(sectionId);
-        if (element) {
-            element.scrollIntoView({ behavior: 'smooth' });
-        }
-    }
+    // const scrollToSection = (sectionId: string) => {
+    //     const element = document.getElementById(sectionId);
+    //     if (element) {
+    //         element.scrollIntoView({ behavior: 'smooth' });
+    //     }
+    // }
 
     return (
-        <section id="home" className='title'>
-            <div>
-                <h1>JOIN THE CLUB!</h1>
+        <section id="home">
+            <div className='left-half'>
+                <div className='title'>
+                    <h1>JOIN THE CLUB!</h1>
+                </div>
+                <p className='description'>
+                    Connect with like-minded game developers, learn new skills, and collaborate on exciting projects!
+                </p>
+                <Button className='btn'>JOIN NOW</Button>
+            </div>
+            <div className='right-half'>
+                <img src='../../public/imgs/mac-falling-transparent.png'/>
             </div>
         </section>
     );
