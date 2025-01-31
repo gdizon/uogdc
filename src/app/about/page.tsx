b5d6bc63259e2b82b916ca1219ac5fda7d307779
gdizon <lainegelic@gmail.com>
2025-01-31 10:12:48 -0500

    about us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,14 +7,26 @@ import Link from 'next/link';
 const About = () => {
     return (
         <section id="about">
-            <Image
-                src='/imgs/Mac/mac-tech.png'
-                alt='About Image'
-                height={500}
-                width={500}
-            />
-            <div >
-
+            <div className='container'>
+                <div className='about-us-image'>
+                    <div className='image'>
+                        <Image
+                            src='/imgs/Mac/mac-tech.png'
+                            alt='About Image'
+                            height={500}
+                            width={500}
+                        />
+                    </div>
+                </div>
+                <div className='about-us-text'>
+                    <h1 className='about-us-title'>ABOUT US</h1>
+                    <p className='text'>
+                    Our game development club is a University of Ottawa community of passionate individuals who come together to create, learn, and share their love for game development.
+                    </p>
+                    <p className='maybe'>
+                        Meet the team →
+                    </p>
+                </div>
             </div>
         </section>
     );
